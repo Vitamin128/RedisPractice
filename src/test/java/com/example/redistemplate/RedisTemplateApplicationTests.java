9bfd64b3db9857fd0532119c50d7177390bb7cf2
chuhao.gan <2729583308@qq.com>
2026-09-05 22:24:44 +0800

    common iteration

--- src/test/java/com/example/redistemplate/RedisTemplateApplicationTests.java
+++ src/test/java/com/example/redistemplate/RedisTemplateApplicationTests.java
@@ -7,14 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.autoconfigure.JacksonProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.Cursor;
-import org.springframework.data.redis.core.ScanOptions;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.connection.stream.RecordId;
+import org.springframework.data.redis.core.*;
 import tools.jackson.databind.json.JsonMapper;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Stream;
 
 @SpringBootTest
 class RedisTemplateApplicationTests {
@@ -55,15 +54,45 @@ class RedisTemplateApplicationTests {
 //                .scan("KEY_2", ScanOptions.scanOptions().build()).match()
 
 //        stringRedisTemplate.opsForSet().
-        SetOperations<String,String>setOps=stringRedisTemplate.opsForSet();
+//        SetOperations<String,> setOps = stringRedisTemplate.opsForStream();
 //        setOps.add("USER_1","Team_1");
 //        setOps.add("USER_1","Team_2");
 //        setOps.add("USER_1","Team_3");
 //        setOps.add("USER_1","Team_4");
+
+//        setOps.add("USER_1","Team_5");
+//        setOps.add("USER_1","Team_6");
+//        setOps.add("USER_1","Team_7");
+//        setOps.add("USER_1","Team_8");
 //
 //        System.out.println(setOps.remove("USER_1","Team_1"));
 //        System.out.println();
 //        setOps.remove("USER_1","Team_2");
+//        System.out.println(setOps.isMember("USER_1","Team_3"));
+//        Map<Object,Boolean>result=setOps.isMember("USER_1","Team_1","Team_2","Team_3");
+//        System.out.println(result);
+//        Long length=setOps.size("USER_1");
+//        System.out.println(length);
+//        String result=setOps.randomMember("USER_1");
+//        List<String>result=setOps.pop("USER_1",2);
+//        Set<String>result=setOps.distinctRandomMembers("USER_1",5);
+//        setOps.add("USER_2","Team_1","Team_2","Team_3","Team_4");
+//        System.out.println(result);
+//        Set<String>result=setOps.intersect("USER_1","USER_2");
+//        setOps.move("USER_2","Team_2","USER_1");
+//        System.out.println(result);
+//        setOps.intersectAndStore("USER_1","USER_2","USER_3");
+//        Long length =setOps.unionAndStore("USER_1","USER_2","USER_4");
+//        Set<String>result=setOps.difference("USER_1","USER_2");
+//        System.out.println(result);
+//        StreamOperations<String,Object,Object>StreamOps=stringRedisTemplate.opsForStream();
+//        RecordId Id=StreamOps.add("STREAM_1",Map.of(
+//                "KEY_1","101",
+//                "KEY_2","102",
+//                "KEY_3","103"
+//        ));
+//        System.out.println(Id);
+
     }
 
 
